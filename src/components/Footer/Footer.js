02a0cc638f3c9e0fce2a6466350b8506948f15bc
BMLS <bmls@BMLSs-MacBook-Pro.local>
2023-05-18 09:03:17 -0400

    improving bg-img performance-adding footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import {
-  MDBFooter
+import {MDBFooter
 } from 'mdb-react-ui-kit';
 
 export default function App() {
   return (
     <MDBFooter className='bg-dark text-white text-center text-md-start'>
       <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-        2023 - <a className='text-white' href=''> LatamMedia</a>
+        2023 - <a className='text-white' href="/"> LatamMedia</a>
       </div>
     </MDBFooter>
   );
